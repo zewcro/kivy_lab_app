25521d9db08998f88044be9096403f6cf8295319
zewcro <theorousseau75@gmail.com>
2022-10-13 23:15:06 +0200

    add examples for stacklayout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from kivy.app import App
+from kivy.metrics import dp
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
@@ -9,6 +11,14 @@ from kivy.uix.widget import Widget
 class MainWidget(Widget): 
     pass
 
+
+class StackLayoutExample(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        for i in range(0,25): 
+            b =  Button(text=str(i+1), size_hint=(None, None), size=(dp(100), dp(100)))
+            self.add_widget(b)
+            
 
 class GridLayoutExample(GridLayout):
     pass
