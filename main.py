2e7cc11912556f3365ed03915848fe96fc53c4fa
zewcro <theorousseau75@gmail.com>
2022-10-17 00:45:55 +0200

    fix error of buidl to build !!!!!

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-from tkinter import Grid
-from xmlrpc.client import Boolean
 from kivy.app import App
 from kivy.metrics import dp
+from kivy.properties import StringProperty, BooleanProperty, ObjectProperty
 from kivy.uix.anchorlayout import AnchorLayout
-from kivy.properties import StringProperty, BooleanProperty
-from kivy.uix.stacklayout import StackLayout
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 
+from navigation_screen_manager import NavigationScreenManager
 
-
-
-
+class MyScreenManager(NavigationScreenManager):
+    pass
 
 class LeLabApp(App):
-    pass
+    manager = ObjectProperty(None)
+    
+    def build(self):
+        self.manager = MyScreenManager()
+        return self.manager
 
 
 LeLabApp().run()
